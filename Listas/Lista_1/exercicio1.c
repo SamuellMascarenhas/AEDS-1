5bd6a7b87c7986338fc6d29266f5eda60bd04b72
Samuel Mascarenhas <132409836+SamuellMascarenhas@users.noreply.github.com>
2024-09-22 09:32:38 -0300

    exercicios1

---
+++ Listas/Lista_1/exercicio1.c
@@ -0,0 +1,23 @@
+//Soma de dois números reais
+
+#include <stdio.h>
+
+int main(){
+
+    printf("Soma de dois números reais\n");
+
+    float a;
+    float b;
+    float soma;
+
+    printf("Digite o primeiro valor: ");
+    scanf(" %f", &a);
+    printf("Digite o segundo valor: ");
+    scanf(" %f", &b);
+
+    soma = a + b;
+
+    printf("A soma do valor %f + o valor %f é = %f", a, b, soma);
+    
+    return 0;
+}
